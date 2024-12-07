c820b989ec77fb3eecd6d3a702f886a500f3213c
Phantom <188271330+phantomspectrumcompressor91975@users.noreply.github.com>
2024-12-07 08:30:03 -0700

    commit

---
+++ RobloxApi.py
@@ -0,0 +1,22 @@
+import json
+from urllib.request import urlopen
+
+
+def GetGroupMemberCount(GroupID: int) -> int | None:
+  with urlopen(f"https://groups.roblox.com/v1/groups/{GroupID}") as response:
+    if response.status == 200:
+      data = json.loads(response.read())
+      return int(data["memberCount"])
+    else:
+      return None
+
+
+def GetUniverseVisits(universeId: int) -> int | None:
+  response = urlopen(
+      f"https://games.roblox.com/v1/games?universeIds={universeId}")
+  if response.status == 200:
+    resJson = json.loads(response.read())
+    data = resJson['data'][0]
+    return int(data['visits'])
+  else:
+    return None
---
+++ main.py
@@ -0,0 +1,71 @@
+from RobloxApi import GetGroupMemberCount, GetUniverseVisits
+
+import discord, os, json
+from discord import Activity, Intents
+from discord.ext import commands, tasks
+
+idle = discord.Status.idle
+online = discord.Status.online
+watch = discord.ActivityType.watching
+client = commands.Bot(command_prefix=None, intents=Intents.default(), status=idle)
+
+visitsChannelId = "None"
+membersChannelId = "None"
+
+robloxGroupId = "None"
+robloxGameId = "None"
+
+
+#on ready
+@client.event
+async def on_ready():
+    print('bot is ready')
+    Ping.start()
+    check.start()
+
+
+@tasks.loop(minutes=1)
+async def Ping():
+    await client.change_presence(activity=Activity(type=watch, name=f"ping: {round(client.latency*1000)}ms"))
+
+
+@tasks.loop(hours=2)
+# Change Number   ^ To Change When The Channels Update
+async def check():
+    await client.change_presence(status=online)
+
+    MembersChannel = client.get_channel(int(visitsChannelId))
+    VisitsChannel = client.get_channel(int(membersChannelId))
+
+    if not robloxGroupId == "None":
+        members = GetGroupMemberCount(robloxGroupId)
+        try:
+            if members is not None:
+                await MembersChannel.edit(name=f'Group Members: {str(members)}'
+                                          )
+                print('updated member channel')
+            else:
+                pass
+        except Exception as e:
+            print(f"cannot update members channel because of {e}")
+    else:
+        pass
+
+
+    if not robloxGameId == "None":
+        visits = GetUniverseVisits(robloxGameId)
+        try:
+            if visits is not None:
+                await VisitsChannel.edit(name=f'Game Visits: {str(visits)}')
+                print('updated visits channel')
+            else:
+                pass
+        except Exception as e:
+            print(f"cannot update visits channel because of {e}")
+    else:
+        pass
+
+    await client.change_presence(status=idle)
+
+
+client.run(os.environ['Token'])
